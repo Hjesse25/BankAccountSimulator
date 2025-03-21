75677b1a0ac13b1af91569457748dfa8b53d4a16
Jesse <196892153+Hjesse25@users.noreply.github.com>
2025-03-20 21:10:44 -0700

    complete bankaccount

---
+++ BankAccount.cs
@@ -0,0 +1,44 @@
+namespace BankAccountSimulator
+{
+    public class BankAccount
+    {
+        private decimal balance;
+        public string Owner { get; set; }
+
+        public BankAccount(string owner, decimal balance)
+        {
+            Owner = owner;
+            Deposit(balance);
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be positive.");
+            }
+
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Withdraw amount must be positive.");
+            }
+
+            if (amount > balance)
+            {
+                throw new InvalidOperationException("Insufficient funds");
+            }
+
+            balance -= amount;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+    }
+}
